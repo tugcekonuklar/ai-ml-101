f3caf8ef897f83d760eb2bf6281bef2c39d0c547
tugcekonuklar <tkonuklar@gmail.com>
2021-02-08 22:36:45 +0100

    feat: add truth table

--- src/com/tkonuklar/Main.java
+++ src/com/tkonuklar/Main.java
@@ -2,4 +2,7 @@ package com.tkonuklar;
 
 public class Main {
 
+    public static void main(String[] args) {
+    }
+
 }
---
+++ src/com/tkonuklar/logic/TruthTable.java
@@ -0,0 +1,22 @@
+package com.tkonuklar.logic;
+
+public class TruthTable {
+
+
+    private static void printTruthTable(int n) {
+        int rows = (int) Math.pow(2, n);
+        System.out.println("Row Count: " + rows);
+        for (int i = 0; i < rows; i++) {
+            System.out.print(String.format("Row %s -> ", i));
+            for (int j = n - 1; j >= 0; j--) {
+                System.out.print((i / (int) Math.pow(2, j)) % 2 + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        printTruthTable(5); //enter any natural int
+    }
+
+}
